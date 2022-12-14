3a591d9e944f817a5ceef957596c4e9524ca5935
Nalbert Cerqueira <nalbertc.p@gmail.com>
2022-12-14 00:17:53 -0300

    changing directory name

---
+++ 01-Calculator/src/components/macos-bar/Macbar.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import "./Macbar.css"
+
+export default function Macbar() {
+    return (
+        <div className="macos-bar">
+            <span className="close-option"></span>
+            <span className="minimize-option"></span>
+            <span className="zoom-control"></span>
+        </div>
+    )
+}
