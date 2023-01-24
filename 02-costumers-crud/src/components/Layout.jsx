b86f12b9977747b1faf5438e98e2de927a4da67f
Nalbert Cerqueira <nalbertc.p@gmail.com>
2023-01-24 19:20:14 -0300

    added scroll-auto in table container

--- 02-costumers-crud/src/components/Layout.jsx
+++ 02-costumers-crud/src/components/Layout.jsx
@@ -7,10 +7,10 @@ export default function Layout(props) {
     return (
         <div
             className="bg-white flex flex-col text-gray-800 w-2/3
-            rounded-xl text-2xl min-w-fit whitespace-nowrap opacity-0 animate-showLayout "
+            rounded-xl text-2xl min-w-fit whitespace-nowrap opacity-0 animate-showLayout max-h-screen"
         >
             <Title>{props.title}</Title>
-            <section className="m-6 overflow-hidden">{props.children}</section>
+            <section className="m-6 overflow-y-auto">{props.children}</section>
         </div>
     )
 }
