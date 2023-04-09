50327121d5336e0d450492158ccc5eefc038f61d
unknown <nalbertc.p@gmail.com>
2023-04-08 22:05:32 -0300

    imported vite-plugin-html to vite configs

--- 05-ecommerce-homepage/vite.config.js
+++ 05-ecommerce-homepage/vite.config.js
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react"
 import autoprefixer from "autoprefixer"
 import { defineConfig } from "vite"
+import { createHtmlPlugin } from "vite-plugin-html"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), createHtmlPlugin({ minify: true })],
     css: {
         postcss: {
             plugins: [autoprefixer()]
