b91852e7a14e434458ec0d67d90301eacf4f4a95
unknown <nalbertc.p@gmail.com>
2023-04-15 13:13:32 -0300

    placed TodoContext in the top of App component

--- 03-todo-app/frontend/src/main/App.jsx
+++ 03-todo-app/frontend/src/main/App.jsx
@@ -1,17 +1,19 @@
 import React from "react"
 import { BrowserRouter } from "react-router-dom"
+import TodoContext from "../contexts/TodoContext.jsx"
 
 import Menu from "../components/Menu.jsx"
 import Routes from "./AppRoutes.jsx"
 
-//Componente principal
 export default function App() {
     return (
-        <main className="px-0 sm:px-12 min-w-360 max-w-7xl m-auto">
-            <BrowserRouter>
-                <Menu />
-                <Routes />
-            </BrowserRouter>
-        </main>
+        <TodoContext>
+            <main className="px-0 sm:px-12 min-w-360 max-w-7xl m-auto">
+                <BrowserRouter>
+                    <Menu />
+                    <Routes />
+                </BrowserRouter>
+            </main>
+        </TodoContext>
     )
 }
