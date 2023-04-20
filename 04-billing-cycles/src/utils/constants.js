e80fbda4271b6610a4acce17c162587e0f4d7f0f
unknown <nalbertc.p@gmail.com>
2023-04-19 23:34:49 -0300

    created basePath variable

--- 04-billing-cycles/src/utils/constants.js
+++ 04-billing-cycles/src/utils/constants.js
@@ -2,5 +2,7 @@ export const defaultCurrency = "R$"
 
 export const baseApiUrl = process.env.NEXT_PUBLIC_BASE_URL
 
+export const basePath = process.env.NEXT_PUBLIC_BASE_PATH
+
 export const defaultFailMessage =
     "Desculpe, ocorreu um erro interno no servidor. Estamos trabalhando para corrigir o problema."
