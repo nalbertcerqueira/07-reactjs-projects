f68c688d145aec4bc52fd59ef6902744ef8e6e80
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-07-07 16:49:10 -0300

    added a useEffect to fetch users information.

--- 04-billing-cycles/src/contexts/providers/UserContext.jsx
+++ 04-billing-cycles/src/contexts/providers/UserContext.jsx
@@ -1,11 +1,27 @@
 import propTypes from "prop-types"
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 
 //Contexto utilizado em UserMenu.jsx para exibir o email e username do usuário
+
 export const UserContext = createContext(null)
 
 export default function UserProvider({ children }) {
     const [user, setUser] = useState({ username: "", email: "" })
+
+    useEffect(() => {
+        fetchUser(setUser)
+    }, [])
+
+    function fetchUser() {
+        fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users`)
+            .then(async (res) => {
+                const user = await res.json()
+                setUser(user)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
 
     return (
         <UserContext.Provider
