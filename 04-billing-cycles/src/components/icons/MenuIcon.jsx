7778a7c8a9f6e390c43a17cc5a2b67969fa56414
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-10-02 19:07:18 -0300

    add aria-label to toggle menu button

--- 04-billing-cycles/src/components/icons/MenuIcon.jsx
+++ 04-billing-cycles/src/components/icons/MenuIcon.jsx
@@ -2,23 +2,21 @@ import propTypes from "prop-types"
 
 export default function MenuIcon(props) {
     return (
-        <div>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-                focusable="false"
-                strokeWidth={1.5}
-                className={`w-7 h-7 ${props.className}`}
-            >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-            </svg>
-        </div>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            focusable="false"
+            strokeWidth={1.5}
+            className={`w-7 h-7 ${props.className}`}
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+        </svg>
     )
 }
 MenuIcon.propTypes = {
--- 04-billing-cycles/src/components/layout/Header.jsx
+++ 04-billing-cycles/src/components/layout/Header.jsx
@@ -6,12 +6,18 @@ import UserMenu from "./UserMenu"
 
 //Componente utilizado em AppTemplate.jsx
 export default function Header({ className }) {
-    const { changeMenuState } = useContext(MenuContext)
+    const { changeMenuState, menu } = useContext(MenuContext)
+    const buttonLabel = menu === "open" ? "recolher menu de navegação" : "exibir menu de navegação"
 
     return (
         <header className={`app-header ${className || ""}`}>
             <nav className="w-full flex items-center self-stretch">
-                <button type="button" onClick={changeMenuState} className="py-2 menu-button">
+                <button
+                    aria-label={buttonLabel}
+                    type="button"
+                    onClick={changeMenuState}
+                    className="py-2 menu-button"
+                >
                     <MenuIcon className="stroke-white" />
                 </button>
                 <UserMenu />
