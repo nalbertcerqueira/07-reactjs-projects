c07072d3e812ff5108e4753abfa585da764929dc
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-10-02 20:02:38 -0300

    add aria-live and aria-atomic to ValidationMsg custom component

--- 04-billing-cycles/src/components/common/ValidationMsg.jsx
+++ 04-billing-cycles/src/components/common/ValidationMsg.jsx
@@ -1,9 +1,15 @@
 import propTypes from "prop-types"
+import { Fragment } from "react"
 
 export default function ValidationMsg({ message, children, className }) {
+    const randomKey = Math.floor(Math.random() * 100000)
     return (
-        <div className={`text-red-500 text-sm animate-[show_0.1s_forwards] ${className || ""}`}>
-            {message || children}
+        <div
+            aria-atomic="true"
+            aria-live="polite"
+            className={`text-red-500 text-sm animate-[show_0.1s_forwards] ${className || ""}`}
+        >
+            <Fragment key={randomKey}>{message || children}</Fragment>
         </div>
     )
 }
